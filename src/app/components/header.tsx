2b5d90bf479111fd4d03a33155552c6247e3b7ab
matheusnff85 <matheus.marinhodsp@gmail.com>
2024-08-22 18:18:50 -0300

    feat: implementa header

---
+++ src/app/components/header.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+export function Header() {
+  return (
+    <header className="fixed inset-x-0 top-0 z-50 backdrop-blur duration-200 bg-zinc-900 shadow-sm shadow-zinc-800">
+      <div className="container flex items-center justify-between p-6 mx-auto">
+        <span className="text-4xl font-bold dark:text-white">
+          Faturar Digital
+        </span>
+      </div>
+    </header>
+  );
+}
