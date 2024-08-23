d04153f782d05a9554b5e343e3d764747f8e27ec
matheusnff85 <matheus.marinhodsp@gmail.com>
2024-08-23 16:08:32 -0300

    refac: troca porta do json-server

--- src/services/api.ts
+++ src/services/api.ts
@@ -19,7 +19,7 @@ interface StoreState {
   filterClientes: (searchTerm: string) => void;
 }
 
-const API_URL = "http://localhost:5000/clientes";
+const API_URL = "http://localhost:5252/clientes";
 
 export const useClienteStore = create<StoreState>((set, get) => ({
   clientes: [],
